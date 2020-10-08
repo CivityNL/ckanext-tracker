04f35403bc6282b108999bf138e236b935fa9861
Gil Hilário <gil@civity.nl>
2020-10-08 16:46:11 +0200

    Update packagetracker_ckantockan_donl_schema.json: added Dutch and Swedish translations (Google translate)

--- ckanext/datastoretracker_geoserver/plugin.py
+++ ckanext/datastoretracker_geoserver/plugin.py
@@ -1,8 +1,17 @@
 import ckanext.datastoretracker.plugin as datastoretracker
 from worker.geoserver import GeoServerWorkerWrapper
+import logging
+
+log = logging.getLogger(__name__)
+
 
 class Datastoretracker_GeoserverPlugin(datastoretracker.DatastoretrackerPlugin):
 
     queue_name = 'geoserver'
     worker = GeoServerWorkerWrapper()
-    
+    geoserver_link_field_name = 'geoserver_link_enabled'
+
+    def after_upload(self, context, resource_dict, dataset_dict):
+        if self.geoserver_link_field_name in dataset_dict and dataset_dict[self.geoserver_link_field_name] == 'True':
+            log.info('Linking to Geoserver')
+            self.put_on_a_queue(context, resource_dict, dataset_dict, self.get_worker().create_datasource)
