37bb01e4f6acbca10731ea6814716cb2674d33ba
erik@civity.nl <erik@civity.nl>
2023-02-27 12:11:22 +0100

    DEV-4841: added tracker-specific context class to keep track of things

---
+++ ckanext/tracker/classes/context.py
@@ -0,0 +1,41 @@
+import logging
+import ckan.plugins as plugins
+
+log = logging.getLogger(__name__)
+
+
+def get_show(object_type, object_id):
+    return plugins.toolkit.get_action('{}_show'.format(object_type))({'ignore_auth': True}, {'id': object_id})
+
+
+class TrackerContext(object):
+
+    def __init__(self, package_id=None):
+        super(TrackerContext, self).__init__()
+        self._before_package = None
+        self._after_package = None
+        self._depth = 0
+        self.before(package_id)
+
+    def before(self, package_id):
+        if package_id is not None:
+            self._before_package = get_show('package', package_id)
+
+    def after(self, package_id):
+        if package_id is not None:
+            self._after_package = get_show('package', package_id)
+
+    def before_package(self):
+        return self._before_package
+
+    def after_package(self):
+        return self._after_package
+
+    def enter(self):
+        self._depth = self._depth + 1
+
+    def exit(self):
+        self._depth = self._depth - 1
+
+    def level(self):
+        return self._depth == 0
